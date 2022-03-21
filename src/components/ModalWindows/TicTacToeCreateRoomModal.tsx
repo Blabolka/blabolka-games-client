b990c451039d4030085a3f492d03c070ce765366
Andrii Klochko <andrii.k@aquimarketing.com>
2022-03-21 21:20:43 +0200

    Work with modal window

--- src/components/ModalWindows/TicTacToeCreateRoomModal.tsx
+++ src/components/ModalWindows/TicTacToeCreateRoomModal.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useAppSelector, useAppDispatch } from '@hooks'
+import React, { useRef, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '@hooks'
 import { setModalWindow } from '@redux-actions/modalWindowActions'
 
 import { TicTacToeGridSizes } from '@entityTypes/ticTacToe'
@@ -7,22 +7,26 @@ import { TicTacToeGridSizes } from '@entityTypes/ticTacToe'
 import './TicTacToeCreateRoomModal.less'
 
 import { makeStyles } from '@mui/styles'
+import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import Button from '@mui/material/Button'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
 import InputLabel from '@mui/material/InputLabel'
 import LoadingButton from '@mui/lab/LoadingButton'
 import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
 
 const useStyles = makeStyles({
-    // Select styles
+    // Input styles
     inputLabel: {
         '&.Mui-focused': {
             color: '#3c988e',
         },
     },
-    select: {
+    input: {
         '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
             borderColor: '#3c988e',
         },
@@ -40,6 +44,24 @@ const useStyles = makeStyles({
             backgroundColor: 'rgba(60, 152, 142, 0.12)',
         },
     },
+
+    // Checkbox
+    checkbox: {
+        '&.Mui-checked': {
+            color: '#3c988e',
+        },
+    },
+
+    textFieldInput: {
+        '& label.Mui-focused': {
+            color: '#3c988e',
+        },
+        '& .MuiOutlinedInput-root': {
+            '&.Mui-focused fieldset': {
+                borderColor: '#3c988e',
+            },
+        },
+    },
 })
 
 const TicTacToeCreateRoomModal = () => {
@@ -48,14 +70,29 @@ const TicTacToeCreateRoomModal = () => {
 
     const classes = useStyles()
 
+    const [buttonIsLoading, setButtonIsLoading] = useState(false)
     const [gridSizeSelectValue, setGridSizeSelectValue] = useState(TicTacToeGridSizes.THREE_BY_THREE)
+    const [isPrivate, setIsPrivate] = useState(false)
+    const inputPasswordRef = useRef({ value: '' })
 
     const onGridSizeSelectChange = (element) => {
         setGridSizeSelectValue(element.target.value)
     }
 
+    const onCheckboxChange = (element) => {
+        setIsPrivate(element.target.checked)
+    }
+
     const onModalClose = () => {
-        dispatch(setModalWindow({ ...modalWindow, isOpen: false }))
+        if (!buttonIsLoading) {
+            dispatch(setModalWindow({ ...modalWindow, isOpen: false }))
+            setGridSizeSelectValue(TicTacToeGridSizes.THREE_BY_THREE)
+            setIsPrivate(false)
+        }
+    }
+
+    const onLoadingButtonClick = () => {
+        setButtonIsLoading(true)
     }
 
     return (
@@ -69,34 +106,58 @@ const TicTacToeCreateRoomModal = () => {
                 <div className="modal-window">
                     <div className="modal-window__header font-size-20px text-title-color">Tic-Tac-Toe Room</div>
                     <div className="modal-window__body">
-                        <FormControl size="small">
-                            <InputLabel className={classes.inputLabel} id="grid-size-select-label">
-                                Grid size
-                            </InputLabel>
-                            <Select
-                                defaultValue={TicTacToeGridSizes.THREE_BY_THREE}
-                                labelId="grid-size-select-label"
-                                label="Grid size"
-                                onChange={onGridSizeSelectChange}
-                                className={classes.select}
-                                inputProps={{
-                                    classes: {
-                                        icon: classes.icon,
-                                    },
-                                }}
-                            >
-                                <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.THREE_BY_THREE}>
-                                    {TicTacToeGridSizes.THREE_BY_THREE}
-                                </MenuItem>
+                        <Box sx={{ paddingBottom: '10px' }}>
+                            <FormControl size="small">
+                                <InputLabel className={classes.inputLabel} id="grid-size-select-label">
+                                    Grid size
+                                </InputLabel>
+                                <Select
+                                    defaultValue={TicTacToeGridSizes.THREE_BY_THREE}
+                                    labelId="grid-size-select-label"
+                                    label="Grid size"
+                                    onChange={onGridSizeSelectChange}
+                                    className={classes.input}
+                                    inputProps={{
+                                        classes: {
+                                            icon: classes.icon,
+                                        },
+                                    }}
+                                >
+                                    <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.THREE_BY_THREE}>
+                                        {TicTacToeGridSizes.THREE_BY_THREE}
+                                    </MenuItem>
 
-                                <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.FIVE_BY_FIVE}>
-                                    {TicTacToeGridSizes.FIVE_BY_FIVE}
-                                </MenuItem>
+                                    <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.FIVE_BY_FIVE}>
+                                        {TicTacToeGridSizes.FIVE_BY_FIVE}
+                                    </MenuItem>
 
-                                <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.SEVEN_BY_SEVEN}>
-                                    {TicTacToeGridSizes.SEVEN_BY_SEVEN}
-                                </MenuItem>
-                            </Select>
+                                    <MenuItem className={classes.menuItem} value={TicTacToeGridSizes.SEVEN_BY_SEVEN}>
+                                        {TicTacToeGridSizes.SEVEN_BY_SEVEN}
+                                    </MenuItem>
+                                </Select>
+                            </FormControl>
+
+                            <FormControlLabel
+                                checked={isPrivate}
+                                onChange={onCheckboxChange}
+                                value="end"
+                                control={<Checkbox className={classes.checkbox} size="small" />}
+                                label="Private Room"
+                                labelPlacement="end"
+                                sx={{ fontSize: '14px', marginLeft: '8px' }}
+                            />
+                        </Box>
+
+                        <FormControl fullWidth>
+                            <TextField
+                                disabled={!isPrivate}
+                                type="password"
+                                label="Password"
+                                variant="outlined"
+                                size="small"
+                                inputRef={inputPasswordRef}
+                                className={classes.textFieldInput}
+                            />
                         </FormControl>
                     </div>
                     <div className="modal-window__actions">
@@ -104,13 +165,15 @@ const TicTacToeCreateRoomModal = () => {
                             Close
                         </Button>
                         <LoadingButton
+                            loading={buttonIsLoading}
+                            color="inherit"
+                            variant="contained"
+                            onClick={onLoadingButtonClick}
                             sx={{
                                 backgroundColor: '#3c988e',
                                 color: '#ffffff',
                                 '&:hover': { backgroundColor: '#348a81' },
                             }}
-                            color="inherit"
-                            variant="contained"
                         >
                             Create Room
                         </LoadingButton>
